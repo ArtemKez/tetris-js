0a93a45c1649320030936bbdd71e744017b0ac93
ArtemKez <artem.kez.work@gmail.com>
2021-01-15 08:15:59 +0200

    start work

--- src/game.js
+++ src/game.js
@@ -1,10 +1,20 @@
 export default class Game {
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200,
+    }
+
     score = 0;
-    lines = 0;
-    level = 0;
+    lines = 19;
     playfield = this.createPlayfield();
     activePiece = this.createPiece();
     nextPiece = this.createPiece();
+
+    get level() {
+        return Math.floor(this.lines * 0.1);
+    }
 
     getState() {
         const playfield = this.createPlayfield();
@@ -26,6 +36,10 @@ export default class Game {
             }
         }
         return {
+            score: this.score,
+            level: this.level,
+            lines: this.lines,
+            nextPiece: this.nextPiece,
             playfield
         }
     }
@@ -133,7 +147,9 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
-            this.updatePieces()
+            const clearLines = this.clearLines();
+            this.ubdateScore(clearLines);
+            this.updatePieces();
         }
     }
 
@@ -196,6 +212,45 @@ export default class Game {
                     this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
                 }
             }
+        }
+    }
+
+    clearLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y >= 0 ; y--) {
+            let numberOfBlocks = 0
+
+            for (let x = 0; x < columns ; x++) {
+                if (this.playfield[y][x]) {
+                    numberOfBlocks += 1;
+                }
+            }
+
+            if (numberOfBlocks === 0) {
+                break;
+            } else if (numberOfBlocks < columns) {
+                continue;
+            } else if (numberOfBlocks === columns) {
+                lines.unshift(y);
+            }
+
+        }
+
+        for (let index of lines) {
+            this.playfield.splice(index, 1);
+            this.playfield.unshift(new Array(columns).fill(0));
+        }
+
+        return lines.length;
+    }
+
+    ubdateScore(cleardLines) {
+        if (cleardLines > 0) {
+            this.score += Game.points[cleardLines] * (this.level +1);
+            this.lines += cleardLines;
         }
     }
 
